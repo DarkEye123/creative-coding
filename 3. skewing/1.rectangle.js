b5d632d639d0bdab27836b0ffb9d88a27e6669cd
DarkEye <lesko.matej.pu@gmail.com>
2023-08-06 19:24:00 +0200

    rectangle API

--- 3. skewing/1.rectangle.js
+++ 3. skewing/1.rectangle.js
@@ -1,13 +1,25 @@
 const canvasSketch = require("canvas-sketch");
 
 const settings = {
+  //good for instagram
   dimensions: [1080, 1080],
 };
 
 const sketch = () => {
+  let x, y, w, h;
   return ({ context, width, height }) => {
     context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
+
+    x = width * 0.5;
+    y = height * 0.5;
+    w = width * 0.6;
+    h = width * 0.1;
+    context.save();
+    context.translate(x, y);
+
+    context.strokeStyle = "blue";
+    context.strokeRect(w * -0.5, h * -0.5, w, h);
   };
 };
 canvasSketch(sketch, settings);
