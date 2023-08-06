70d300a4a2d598e7b9a6667b172d81cf305b8781
DarkEye <lesko.matej.pu@gmail.com>
2023-08-06 19:12:55 +0200

    rectangle -init

---
+++ 3. skewing/1.rectangle.js
@@ -0,0 +1,11 @@
+const canvasSketch = require("canvas-sketch");
+
+const settings = [1080, 1080];
+
+const sketch = () => {
+  return ({ context, width, height }) => {
+    context.fillStyle = "white";
+    context.fillRect(0, 0, width, height);
+  };
+};
+canvasSketch(sketch, settings);
